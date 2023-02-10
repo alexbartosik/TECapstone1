39932de691df47441935e0be9c6d74f8b37a037c
Josh Mettle <mettle.josh@gmail.com>
2023-02-10 15:32:08 -0500

    Added select product and make change

--- Additional note on reading and writing files.md
+++ Additional note on reading and writing files.md
@@ -4,3 +4,8 @@
 2. (Required) Copy the inventory.csv file to that directory and read only from that copy of the file
 3. (Required) When writing the log file, write it to the "C:\Store" directory that you created in step #1
 4. (Optional) If you undertake the optional portion of the mini-capstone (reading and writing a TotalSystemSalesReport ), that file should be in the "C:\Store" folder created in step #1 
+
+Follow up
+    Inventory Test - Fix
+    Add Money UI - Add error for negitive int number
+    UI CartSwitch Case - Add result for 0 to back out.
--- mini-capstone/CapstoneTests/InventoryTests.cs
+++ mini-capstone/CapstoneTests/InventoryTests.cs
@@ -48,7 +48,8 @@ namespace CapstoneTests
         [DataRow("HC", "H1", "test", 1.00, "test")]
         [DataRow("LI", "L1", "test", 1.00, "test")]
         
-        public void CreateCandyFromImportCreatesCandy(string candyType, string id, string name, double price, string wrapped)
+
+        public void CreateCandyFromImportCreatesCorrectCandy(string candyType, string id, string name, double price, string wrapped)
         {
             // arrange
             decimal dprice = (decimal)price;
@@ -80,5 +81,83 @@ namespace CapstoneTests
             // assert
             Assert.AreEqual(0, testList.Count);
         }
+
+        [TestMethod]
+        [DataRow("CH","C1", "C1")]
+        public void CheckInventoryIdReturnsProperCandyId(string classId,string id, string expectResult)
+        {
+            // Arrange
+            Inventory testInventory = new Inventory();
+            testInventory.CreateCandyFromImport(classId, id, "Test", 1, "T");
+
+            // Act
+            Candy resultCandy = testInventory.CheckInventoryId(id);
+
+            // Assert
+            Assert.AreEqual(expectResult, resultCandy.Id);
+        }
+
+        [TestMethod]
+        [DataRow("QQ", "C1", "C1")]
+        public void CheckInventoryIdReturnsNullIfCalledIdDoesNotExistInInventory(string classId, string id, string expectResult)
+        {
+            // Arrange
+            Inventory testInventory = new Inventory();
+            testInventory.CreateCandyFromImport(classId, id, "Test", 1, "T");
+
+            // Act
+            Candy resultCandy = testInventory.CheckInventoryId(id);
+
+            // Assert
+            Assert.IsNull(resultCandy);
+        }
+
+        [TestMethod]
+        [DataRow(10,90)]
+        public void CheckRemoveInventoryProperlyDecrementsInventoryQty(int quantityToRemove, int expectedInventoryQty)
+        {
+            // Arrange
+            Inventory testInventory = new Inventory(); //Create inventory
+            testInventory.CreateCandyFromImport("CH", "C1", "Test", 1, "T"); // Add candy to inventory w/ quantity 100
+            Candy testCandy = testInventory.CheckInventoryId("C1"); // Create candy
+            int actualInventory = 0;
+
+            // Act
+            testInventory.RemoveFromInventory(testCandy, quantityToRemove); // call test method
+            List<KeyValuePair<Candy, int>> testList = testInventory.ListOfCurrentInventory(); // create list of current inventory values
+            foreach(KeyValuePair<Candy,int> candy in testList) // find the quantity in the list
+            {
+                if(candy.Key == testCandy)
+                {
+                    actualInventory = candy.Value;
+                }
+            }
+
+            // Assert
+            Assert.AreEqual(expectedInventoryQty, actualInventory); 
+        }
+
+
+        /*
+        [TestMethod] // Finish after decrement inventory method
+        [DataRow(100,2)]
+        [DataRow(101,)]
+        [DataRow(-1,)]
+
+        public void CheckInventoryQtyReturnsProperCondition(int testQty, int expectResult)
+        {
+            // Arrange
+            Inventory testInventory = new Inventory();
+            testInventory.CreateCandyFromImport("CH", "C1", "Test", 1, "T");
+            Candy testCandy = testInventory.CheckInventoryId("C1");
+
+            // Act
+            int result = testInventory.CheckInventoryQty(testCandy, testQty);
+
+            // Assert
+
+
+        }
+        */
     }
 }
